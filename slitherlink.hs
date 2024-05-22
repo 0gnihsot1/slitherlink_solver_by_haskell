2354ddcff4338381e80dc8833f28f34f3c3f73b9
zn8gmKoMxbT$ye <64427356+0gnihsot1@users.noreply.github.com>
2024-05-22 21:43:45 +0900

    修正

--- slitherlink.hs
+++ slitherlink.hs
@@ -24,7 +24,6 @@ getNum :: Board -> Int -> Int -> Int
 getNum board x y = (board !! y) !! x
 
 -- 指定位置の周囲のライン数を取得する
-getLineCnt :: LineBoard -> Int -> Int -> Int
 getLineCnt board x y = sum $ concatMap (f x') $ f y' board 
   where x' = x * 2
         y' = y * 2
@@ -32,9 +31,12 @@ getLineCnt board x y = sum $ concatMap (f x') $ f y' board
 
 -- ラインの初期状態を取得する
 getInitLineBoard :: Int -> Int -> LineBoard
-getInitLineBoard width height = replicate (height * 2 + 1) (replicate (width * 2 + 1) 0)
+getInitLineBoard x y = getArr y $ getArr x 0
+  where getArr 0 _ = []
+        getArr n a = a : getArr (n-1) a
 
 -- 展開する
+expand :: [[a]] -> [a]
 expand [] = []
 expand (x:xs) = x ++ expand xs
 
@@ -47,7 +49,8 @@ expand (x:xs) = x ++ expand xs
 -- 最初の数字の場所を取得する
 getFirstNumPosition :: Board -> (Int, Int)
 getFirstNumPosition board = (pos `mod` 10, pos `div` 10)
-  where pos = length $ takeWhile (\x -> x == 9 || x == 0) (expand board)
+  where pos = length $ takeWhile checkEnable (expand board)
+        checkEnable = \x -> x == 9 || x == 0
 
 -- 解法
 --solver :: Board -> [Board]
