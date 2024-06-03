fd33b9d5d564e1ba3e066f921e6198bfed6b8698
zn8gmKoMxbT$ye <mina_olen_shingo@yahoo.co.jp>
2024-06-03 19:52:30 +0900

    Position定義に変更

--- slitherlink.hs
+++ slitherlink.hs
@@ -5,15 +5,17 @@ import Control.Monad
 type Board = [[Int]]
 -- ラインの盤面
 type LineBoard = [[Int]]
+-- ポジション
+type Position = (Int, Int)
 
 -- 数字を取得する
 -- 枠をはみ出した場合はエラーとなる
-getNum :: Board -> Int -> Int -> Int
-getNum board x y = board !! y !! x
+getNum :: Board -> Position -> Int
+getNum board (x, y) = board !! y !! x
 
 -- ラインを引く
-putLine :: LineBoard -> Int -> Int -> LineBoard
-putLine board x y = subst board y $ subst (board !! y) x 1
+putLine :: LineBoard -> Position -> LineBoard
+putLine board (x, y) = subst board y $ subst (board !! y) x 1
   where subst [] _ _ = []
         subst (x:xs) 0 m = m : xs
         subst (x:xs) n m = x : subst xs (n - 1) m
@@ -27,10 +29,10 @@ getHeight :: Board -> Int
 getHeight board = length board
 
 -- 指定位置の周囲のライン数を取得する
-getLineCnt :: LineBoard -> Int -> Int -> Int
-getLineCnt board x y = sum $ map f makeIdx
+getLineCnt :: LineBoard -> Position -> Int
+getLineCnt board (x, y) = sum $ map f makeIdx
   where makeIdx = [(0,-1), (1,0), (0,1), (-1,0)]
-        f (x', y') = getNum board (x + x') (y + y')
+        f (x', y') = getNum board ((x + x'), (y + y'))
 
 -- ラインの初期状態を取得する
 getInitLineBoard :: Board -> LineBoard
@@ -48,11 +50,11 @@ checkAllNumSatisfied board lineBoard = foldr f True makeIdx
   where makeIdx = [(x, y) | 
                     x <- [0..getWidth board - 1],
                     y <- [0..getHeight board - 1],
-                    getNum board x y < 4]
-        f (x, y) a = getLineCnt lineBoard x y == getNum board x y
+                    getNum board (x, y) < 4]
+        f (x, y) a = getLineCnt lineBoard (x, y) == getNum board (x, y)
 
 -- 最初の数字の場所を取得する
-getFirstNumPosition :: Board -> (Int, Int)
+getFirstNumPosition :: Board -> Position
 getFirstNumPosition board = (pos `mod` width, pos `div` height)
   where pos = length $ takeWhile checkEnable (expand board)
         checkEnable = \x -> x == 9 || x == 0
@@ -62,20 +64,21 @@ getFirstNumPosition board = (pos `mod` width, pos `div` height)
         expand (x:xs) = x ++ expand xs
 
 -- 引いたラインの両脇の整合性をチェックする
-checkLineIntegrity :: LineBoard -> Int -> Int -> Bool
-checkLineIntegrity lineBoard x y = 
-  if odd y then check (x-1) y && check (x+1) y
-  else check x (y-1) && check x (y+1)
-  where check (-1) _ = True
-        check _ (-1) = True
-        check x' y' = getLineCnt lineBoard x' y' <= getNum lineBoard x' y'
+--checkLineIntegrity :: LineBoard -> Position -> Bool
+checkLineIntegrity lineBoard (x, y) = 
+  if odd y then check ((x-1), y) && check ((x+1), y)
+  else check (x, (y-1)) && check (x, (y+1))
+  where check ((-1), _) = True
+        check (_, (-1)) = True
+        check (x', y') = getLineCnt lineBoard (x', y') <= getNum lineBoard (x' ,y')
 
 -- 引いたラインが結合したかをチェックする
-checkLineLinked board x y = (sum $ map f makeIdx) == 2
+checkLineLinked :: LineBoard -> Position -> Bool
+checkLineLinked board (x, y) = (sum $ map f makeIdx) == 2
   where makeIdx = 
           if odd y then [(1,-1), (2,0), (1, 1), (-1,1), (-2, 0), (-1,-1)]
           else [(0,-2), (1,-1), (1,1), (0,2), (-1,1), (-1,-1)]
-        f (x', y') = getNum board (x + x') (y + y') 
+        f (x', y') = getNum board ((x + x'), (y + y'))
 
 -- 解法
 --solver :: Board -> LineBoard
@@ -90,22 +93,21 @@ checkLineLinked board x y = (sum $ map f makeIdx) == 2
 -- 6.全数字に対して線が引かれているかチェック
 --    →Trueならば回答する
 --    →Falseならば次のラインを引く(3)
-solver board = solve1
-  where lineBoard = getInitLineBoard board
-        solve1 = solve2 lineBoard (getFirstNumPosition lineBoard)
-        solve2 board (x, y) = solve3 board x y makeIdx
-        solve3 board x y ((x', y'):idx) = do
+solver board = solve1 (getInitLineBoard board)
+  where solve1 lineBoard = solve2 lineBoard (getFirstNumPosition lineBoard)
+        solve2 lineBoard (x, y) = solve3 lineBoard (x, y) makeIdx
+        solve3 lineBoard (x, y) ((x', y'):idx) = do
           let targetX = x + x'
               targetY = y + y'
-              tmpLineBoard = putLine board targetX targetY
-              makeNewIdx =
+              tmpLineBoard = putLine lineBoard (targetX, targetY)
+              makeTmpIdx = 
                 if odd targetY then [(1,-1), (2,0), (1, 1), (-1,1), (-2, 0), (-1,-1)]
                 else [(0,-2), (1,-1), (1,1), (0,2), (-1,1), (-1,-1)]
-          guard (board !! targetY !! targetX == 0)
-          guard (checkLineIntegrity tmpLineBoard targetX targetY)
-          guard (checkLineLinked tmpLineBoard targetX targetY)
+          guard (lineBoard !! targetY !! targetX == 0)
+          guard (checkLineIntegrity tmpLineBoard (targetX, targetY))
+          guard (checkLineLinked tmpLineBoard (targetX, targetY))
           if checkAllNumSatisfied board tmpLineBoard then return tmpLineBoard
-          else solve3 tmpLineBoard targetX targetY makeNewIdx
+          else solve3 tmpLineBoard (targetX, targetY) makeTmpIdx
         makeIdx = [(0,-1), (1,0), (0,1), (-1,0)]
 
 -- 問題
